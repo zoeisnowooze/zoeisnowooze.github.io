2fbc297d4eaa3f05ee51220ca37c4c3d906ab2f1
Zoé Cassiopée Gauthier <hello@blorp.dev>
2022-04-26 19:08:15 -0400

    Change font to Iosevka Aile and tweak layout

--- .eleventy.js
+++ .eleventy.js
@@ -27,6 +27,7 @@ module.exports = function(eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addPassthroughCopy("img");
 
   return {
